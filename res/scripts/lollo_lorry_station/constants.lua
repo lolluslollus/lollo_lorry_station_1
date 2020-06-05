925b246a422e289f3e30d9b02f2dce29ab0a10ae
lolluslollus <lolluslollus@hotmail.com>
2020-06-05 19:13:01 +0200

    clean

--- res/scripts/lollo_lorry_station/constants.lua
+++ res/scripts/lollo_lorry_station/constants.lua
@@ -9,6 +9,10 @@ local constants = {
 	yTransfFactor = 10,
 	cargoAreaTag = 'lollo_cargo_area',
 	lorryBayTag = 'lollo_lorry_bay',
+	cargoAreaModelFileName = 'lollo_models/lollo_cargo_area.mdl',
+	cargoAreaModuleType = 'lollo_street_terminal_cargo_area',
+	lorryBayModelFileName = 'lollo_models/lollo_lorry_bay.mdl',
+	lorryBayModuleType = 'lollo_street_terminal_lorry_bay',
 }
 
 return constants
