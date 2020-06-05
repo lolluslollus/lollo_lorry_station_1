7f037bc5c341c5d2066d52f32e07a84b8c582469
lolluslollus <lolluslollus@outlook.com>
2020-06-05 10:49:23 +0200

    fix mangle and demangle - again

--- res/scripts/lollo_lorry_station/scratch.lua
+++ res/scripts/lollo_lorry_station/scratch.lua
@@ -23,14 +23,62 @@ local _constants = arrayUtils.addProps(
 	},
 	_modConstants
 )
-
-local idBasesSorted = {}
+local _idBasesSortedDesc = {}
 for k, v in pairs(_constants.idBases) do
-    table.insert(idBasesSorted, {id = v, name = k})
+    table.insert(_idBasesSortedDesc, {id = v, name = k})
 end
-arrayUtils.sort(idBasesSorted, 'id', false)
-for k, v in pairs(idBasesSorted) do
-    local vvv = v
-    local kkk = k
+arrayUtils.sort(_idBasesSortedDesc, 'id', false)
+
+local MangleId = function(x, y, baseId)
+	return baseId + _constants.idFactorY * (y  - _constants.yMin) + (x  - _constants.xMin)
 end
+
+-- result.DemangleId = function(slotId)
+-- 	local rem = slotId % 100
+-- 	local variant = rem
+-- 	slotId = (slotId - rem) / 100
+-- 	rem = slotId % 2000
+-- 	local coordI = rem - 100
+-- 	slotId = (slotId - rem) / 2000
+-- 	return coordI, slotId - 100, variant
+-- end
+
+local DemangleId = function(slotId)
+	-- debugger()
+	local function _getIdBase(slotId)
+		local baseId = 0
+		for _, v in pairs(_idBasesSortedDesc) do
+			if slotId >= v.id then
+				baseId = v.id
+				break
+			end
+		end
+
+		return baseId > 0 and baseId or false
+	end
+
+	local baseId = _getIdBase(slotId)
+	if not baseId then return false, false, false end
+
+	local y = math.floor((slotId - baseId) / _constants.idFactorY)
+	local x = math.floor((slotId - baseId - y * _constants.idFactorY)) + _constants.xMin
+
+	y = y + _constants.yMin
+
+	return x, y, baseId
+end
+
+local mangledIds = {}
+for x = _constants.xMin, _constants.xMax do
+	for y = _constants.yMin, _constants.yMax do
+		mangledIds[#mangledIds + 1] = MangleId(x, y, _constants.idBases.areaSlotIdBase)
+	end
+end
+
+local demangledIds = {}
+for i = 1, #mangledIds do
+	local x, y, baseId = DemangleId(mangledIds[i])
+	demangledIds[#demangledIds + 1] = {x, y, baseId}
+end
+
 local par = { lollo = true }
