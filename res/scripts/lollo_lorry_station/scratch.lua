2ee52313a994d44cee90c186f856c13671676762
lolluslollus <lolluslollus@outlook.com>
2020-06-05 02:38:56 +0200

    fixes

--- res/scripts/lollo_lorry_station/scratch.lua
+++ res/scripts/lollo_lorry_station/scratch.lua
@@ -1,26 +1,36 @@
 package.path = package.path .. ';res/scripts/?.lua'
 
-local dump = require('res/scripts/luadump')
+local luadump = require('lollo_lorry_station/luadump')
+local arrayUtils = require('lollo_lorry_station/arrayUtils')
 local stringUtils = require('lollo_lorry_station/stringUtils')
 
-local function isBuildingStreetSplitter(param)
-    print('\n - param.proposal.toAdd = ')
-    local toAdd = type(param) == 'table' and param
-    and type(param.proposal) == 'userdata' and param.proposal
-    and type(param.proposal.toAdd) == 'userdata' and param.proposal.toAdd
-    print('- toAdd = ', dump(false)(toAdd))
+local _modConstants = require('lollo_lorry_station/constants')
 
-    if toAdd and #toAdd > 0 then
-        for i = 1, #toAdd do
-            if toAdd[i].fileName == [[lollo_street_splitter.con]] then
-                return true
-            end
-        end
-    end
-    return false
+local _constants = arrayUtils.addProps(
+	{
+		cargoAreaSpacing = {0.0, 0.0, 0.0, 0.0}, -- {5, 5, 5, 5},
+		-- the smaller these guys, the closer to the road I can fill a slot without it turning red.
+		-- negative values don't matter, the api takes up the abs value
+		-- cargoPathSpacing = {0.1, 0.1, 0.1, 0.1},
+		cargoPathSpacing = {0.0, 0.0, 0.0, 0.0},
+		idBases = { -- LOLLO NOTE keep this sorted descending
+			areaSlotIdBase = 120000,
+            pathSlotIdBase = 110000,
+            lollo = 1333,
+            lalla = 999999999999
+		},
+		idFactorY = 100
+	},
+	_modConstants
+)
+
+local idBasesSorted = {}
+for k, v in pairs(_constants.idBases) do
+    table.insert(idBasesSorted, {id = v, name = k})
 end
-
+arrayUtils.sort(idBasesSorted, 'id', false)
+for k, v in pairs(idBasesSorted) do
+    local vvv = v
+    local kkk = k
+end
 local par = { lollo = true }
-print(isBuildingStreetSplitter(par))
-
-print(not(qqqqq))
