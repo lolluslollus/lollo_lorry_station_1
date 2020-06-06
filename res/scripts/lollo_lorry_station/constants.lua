ce80a3591131e601fefc37693faff7b09245ac56
lolluslollus <lolluslollus@hotmail.com>
2020-06-07 00:55:21 +0200

    add road connection, wip

--- res/scripts/lollo_lorry_station/constants.lua
+++ res/scripts/lollo_lorry_station/constants.lua
@@ -10,12 +10,14 @@ local constants = {
 	cargoAreaTag = 'lollo_cargo_area',
 	lorryBayTag = 'lollo_lorry_bay',
 	markTag = 'lollo_mark',
+	roadConnectionTag = 'lollo_road_connection',
 	cargoAreaModelFileName = 'lollo_models/lollo_cargo_area.mdl',
 	cargoAreaModuleType = 'lollo_street_terminal_cargo_area',
 	lorryBayModelFileName = 'lollo_models/lollo_lorry_bay.mdl',
 	lorryBayModuleType = 'lollo_street_terminal_lorry_bay',
 	markModelFileName = 'lollo_models/lollo_mark.mdl',
 	constructionFileName = 'station/street/lollo_lorry_station.con',
+	roadConnectionFileName = 'lollo_models/lollo_road_connection.mdl'
 }
 
 return constants
