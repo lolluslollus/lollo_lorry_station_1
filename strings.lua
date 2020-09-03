af0f6acd466de6f174f3fa6637e871bffd50eeaf
lolluslollus <lolluslollus@outlook.com>
2020-09-03 17:36:52 +0200

    docu

--- strings.lua
+++ strings.lua
@@ -8,9 +8,9 @@ function data()
 				
 				To place one, make the streets first and leave some empty space for the station.
 
-				This mod requires the street fine tuning. It provides extra street types, junctions and a slicer tool to avoid destroying too many buildings.
+				This mod requires the street fine tuning, which provides extra street types, junctions and a slicer tool to avoid destroying too many buildings.
 
-				At the current state of the game, I could not find a way to make this station entirely non-destructive: if you have buildings around, some will have to go.
+				At the current state of the game, I could not find a way to make this station entirely non-destructive: if you have buildings around, some will have to go. They should regrow though, let me know.
 				]],
 			["_NAME"] = "Modular streetside lorry station"
 		},
