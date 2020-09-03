ebc6b9b7c69730de45b16099f54d994ed778a982
lolluslollus <lolluslollus@outlook.com>
2020-09-03 17:30:02 +0200

    post mod into steam

--- strings.lua
+++ strings.lua
@@ -3,11 +3,13 @@ function data()
 		en = {
 			["_DESC"] = [[
 				A modular lorry station with one single terminal.
-				You can combine several of these stations to make a complex lorry station, with full control on the paths and the capacities.
+
+				You can combine several of these to make a complex lorry station, with full control on the paths and the capacities.
 				
 				To place one, make the streets first and leave some empty space for the station.
 
-				This mod requires the street fine tuning. It provides extra straat types, junctions and a slicer tool to avoid destroying too many buildings.
+				This mod requires the street fine tuning. It provides extra street types, junctions and a slicer tool to avoid destroying too many buildings.
+
 				At the current state of the game, I could not find a way to make this station entirely non-destructive: if you have buildings around, some will have to go.
 				]],
 			["_NAME"] = "Modular streetside lorry station"
