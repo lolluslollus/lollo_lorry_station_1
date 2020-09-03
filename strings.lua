acdf1c3aebefb0992dfbff78f17d45893b82970a
lolluslollus <lolluslollus@outlook.com>
2020-09-03 20:49:49 +0200

    docu

--- strings.lua
+++ strings.lua
@@ -9,6 +9,8 @@ function data()
 				To place one, make the streets first and leave some empty space for the station.
 
 				This mod requires the street fine tuning, which provides extra street types, junctions and a slicer tool to avoid destroying too many buildings.
+				
+				If you want to force something into a station catchment area, use the ultrathin paths.
 
 				At the current state of the game, I could not find a way to make this station entirely non-destructive: if you have buildings around, some will have to go. They should regrow though, let me know.
 				]],
