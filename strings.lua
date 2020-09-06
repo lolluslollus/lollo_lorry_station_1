a0bae1dbd9c02af2f3e199abde2e5c55759cfb4b
lolluslollus <lolluslollus@hotmail.com>
2020-09-06 13:05:33 +0200

    Update strings.lua

--- strings.lua
+++ strings.lua
@@ -7,7 +7,8 @@ function data()
 				You can combine several of these to make a complex lorry station, with full control on the paths and the capacities.
 				
 				To place one, make the streets first and leave some empty space for the station. Help yourself with the slicer, you will find it in the street construction menu.
-				Streetside cargo areas are designed to overlap with the street side. If you want to alter the underlying road, you must remove them, alter the road and plop them back in, to avoid the collision warning.
+                
+                Streetside cargo areas are designed to overlap with the street side. If you want to alter the underlying road, you must remove them, alter the road and plop them back in, to avoid the collision warning.
 
 				If you want to force something into a station catchment area, use the ultrathin paths.
 
