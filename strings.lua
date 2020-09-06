be0755518b92685534da317a8749b0da1e8e7040
lolluslollus <lolluslollus@hotmail.com>
2020-09-06 13:13:44 +0200

    Update strings.lua

--- strings.lua
+++ strings.lua
@@ -6,13 +6,19 @@ function data()
 
 				You can combine several of these to make a complex lorry station, with full control on the paths and the capacities.
 				
-				To place one, make the streets first and leave some empty space for the station. Help yourself with the slicer, you will find it in the street construction menu.
+				To place one, make the streets first and leave some empty space for the station, which comes with its own piece of road attached. Help yourself with the slicer, you will find it in the street construction menu.
                 
-                Streetside cargo areas are designed to overlap with the street side. If you want to alter the underlying road, you must remove them, alter the road and plop them back in, to avoid the collision warning.
+				You can use the pavement to store cargo or leave it clear for pedestrians.
+                
+				TIPS AND TRICKS
+				Streetside cargo areas are designed to overlap with the street side. If you want to alter the underlying road, you must remove them, alter the road and plop them back in, to avoid the collision warning.
 
 				If you want to force something into a station catchment area, use the ultrathin paths.
+                
+				Use the categories in the street menu so you don't go bananas selecting road types.
 
-				WORD OF WARNING: the first iteration of this mod had an awkward grid. If you have already built some of these stations, they will keep working. As soon as you configure them, the old modules will go away and you will have to plop the new ones instead. It is a one-off and a small price to pay for a decidedly better grid.
+				WORD OF WARNING
+				The first iteration of this mod had an awkward grid. If you have already built some of these stations, they will keep working. As soon as you configure them, the old modules will go away and you will have to plop the new ones instead. It is a one-off and a small price to pay for a decidedly better grid.
 			]],
 			["_NAME"] = "Modular streetside lorry station"
 		},
