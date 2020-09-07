8512633ff6c3e5c32b9b63121799bed98403341f
lolluslollus <lolluslollus@outlook.com>
2020-09-07 20:42:11 +0200

    unbump version

--- mod.lua
+++ mod.lua
@@ -54,7 +54,7 @@ function data()
     end
     return {
         info = {
-            minorVersion = 6,
+            minorVersion = 5,
             severityAdd = 'NONE',
             severityRemove = 'WARNING',
             name = _('_NAME'),
