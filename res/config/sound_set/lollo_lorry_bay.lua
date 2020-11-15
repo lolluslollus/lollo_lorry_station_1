46593e714d67322a72587627c2f96a2d9ad1830b
lolluslollus <lolluslollus@outlook.com>
2020-11-15 19:46:31 +0100

    softer sounds

--- res/config/sound_set/lollo_lorry_bay.lua
+++ res/config/sound_set/lollo_lorry_bay.lua
@@ -3,9 +3,9 @@ local soundsetutil = require "soundsetutil"
 local randomEventNames = {
     "car_idle.wav",
     "selected_bus.wav",
-    "selected_busdepot3.wav",
+    -- "selected_busdepot3.wav",
     "selected_bus_depot.wav",
-    "selected_cargotruckstation2.wav",
+    -- "selected_cargotruckstation2.wav",
     "selected_industrial.wav",
 }
 
@@ -16,14 +16,14 @@ local data = soundsetutil.makeSoundSet()
 -- soundsetutil.addTrackParam01(data, "car_horn.wav", 25.0,
 -- 		{ { .0, .5 } }, { { .0, 0.7 } }, "cargo01")
 		
-soundsetutil.addEventParam01(data, "random32", randomEventNames, 50.0,
-		{ { .33, 1.0 }, { .34, .0 } }, { { .0, 1.0 } }, "cargo01")
+soundsetutil.addEventParam01(data, "random32", randomEventNames, 25.0,
+		{ { 0.33, 1.0 }, { 0.34, 0.0 } }, { { 0.0, 1.0 } }, "cargo01")
 		
-soundsetutil.addEventParam01(data, "random16", randomEventNames, 50.0,
-		{ { 0.33, 0.0 }, { 0.34, 1.0 }, { 0.66, 1.0 }, { 0.67, 0.0 } }, { { .0, 1.0 } }, "cargo01")
+soundsetutil.addEventParam01(data, "random16", randomEventNames, 25.0,
+		{ { 0.33, 0.0 }, { 0.34, 1.0 }, { 0.66, 1.0 }, { 0.67, 0.0 } }, { { 0.0, 1.0 } }, "cargo01")
 		
-soundsetutil.addEventParam01(data, "random8", randomEventNames, 50.0,
-		{ { 0.66, 0.0 }, { 0.67, 1.0 } }, { { .0, 1.0 } }, "cargo01")
+soundsetutil.addEventParam01(data, "random8", randomEventNames, 25.0,
+		{ { 0.66, 0.0 }, { 0.67, 1.0 } }, { { 0.0, 1.0 } }, "cargo01")
 
 return data
 
