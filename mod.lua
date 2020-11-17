a2c460f38adae61d3a31e7f829f95955c901feea
lolluslollus <lolluslollus@outlook.com>
2020-11-17 16:29:09 +0100

    clean

--- mod.lua
+++ mod.lua
@@ -2,56 +2,6 @@ local moduleHelpers = require('lollo_lorry_station.moduleHelpers')
 local streetUtils = require('lollo_lorry_station.streetUtils')
 
 function data()
-    local function _getUiTypeNumber(uiTypeStr)
-        if uiTypeStr == 'BUTTON' then return 0
-        elseif uiTypeStr == 'SLIDER' then return 1
-        elseif uiTypeStr == 'COMBOBOX' then return 2
-        elseif uiTypeStr == 'ICON_BUTTON' then return 3 -- double-check this
-        elseif uiTypeStr == 'CHECKBOX' then return 4 -- double-check this
-        else return 0
-        end
-    end
-
-    local function _addAvailableConstruction(oldFileName, newFileName, scriptFileName, availability, params, streetData)
-        local staticCon = api.res.constructionRep.get(api.res.constructionRep.find(oldFileName))
-        local newCon = api.type.ConstructionDesc.new()
-        newCon.fileName = newFileName
-        newCon.type = staticCon.type
-        newCon.description = staticCon.description
-        newCon.description.name = 'Lollo roadside lorry bay with postRunFn'
-        -- newCon.availability = { yearFrom = 1925, yearTo = 0 } -- this dumps, the api wants it different
-        newCon.availability.yearFrom = availability.yearFrom
-        newCon.availability.yearTo = availability.yearTo
-        newCon.buildMode = staticCon.buildMode
-        newCon.categories = staticCon.categories
-        newCon.order = staticCon.order
-        newCon.skipCollision = staticCon.skipCollision
-        newCon.autoRemovable = staticCon.autoRemovable
-        newCon.soundConfig = staticCon.soundConfig
-        for _, par in pairs(params) do
-            local newConParam = api.type.ScriptParam.new()
-            newConParam.key = par.key
-            newConParam.name = par.name
-            newConParam.tooltip = par.tooltip or ''
-            newConParam.values = par.values
-            newConParam.defaultIndex = par.defaultIndex or 0
-            newConParam.uiType = _getUiTypeNumber(par.uiType)
-            if par.yearFrom ~= nil then newConParam.yearFrom = par.yearFrom end
-            if par.yearTo ~= nil then newConParam.yearTo = par.yearTo end
-            newCon.params[#newCon.params + 1] = newConParam -- the api wants it this way, all the table at once dumps
-        end
-        newCon.updateScript.fileName = scriptFileName .. '.updateFn'
-        newCon.updateScript.params = {
-            globalStreetData = streetData
-        }
-        newCon.preProcessScript.fileName = scriptFileName .. '.preProcessFn'
-        newCon.upgradeScript.fileName = scriptFileName .. '.upgradeFn'
-        newCon.createTemplateScript.fileName = scriptFileName .. '.createTemplateFn'
-
-        print('LOLLO newCon = ')
-        debugPrint(newCon)
-        api.res.constructionRep.add(newCon.fileName, newCon, true) -- fileName, resource, visible
-    end
     return {
         info = {
             minorVersion = 12,
