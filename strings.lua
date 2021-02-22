cd095ffc3c5df33609a8d9861a3c1c915eecd528
lolluslollus <lolluslollus@outlook.com>
2021-02-22 23:47:29 +0100

    Update strings.lua

--- strings.lua
+++ strings.lua
@@ -4,14 +4,14 @@ function data()
 			["_DESC"] = [[
 				A modular lorry station with one single terminal.
 
-				You can combine several of these to make a complex lorry station, with full control on the paths and the capacities.
+				You can combine several of these to make a complex lorry station, with full control on the paths and the capacities. You can also combine them with the stock streetside stations, used as unload-only points for your hub.
 				
-				To place one, make the streets first and leave some empty space for the station, which comes with its own piece of road attached. Help yourself with the slicer, you will find it in the street construction menu if you install the street fine tuning.
+				To place one, make the streets first and leave some empty space for the station, which comes with its own piece of road attached. Best: make the streets with no gaps and help yourself with the slicer, you will find it in the street construction menu if you install the street fine tuning.
                 
 				You can use the pavement to store cargo or leave it clear for pedestrians.
                 
 				TIPS AND TRICKS
-				Placing two stations close to each other can be a pain, because the game tries to snap things together, fails and raises pointless error messages. Add some extra length to get past this.
+				Placing two stations close to each other can be a pain, because the game tries to snap things together, fails and raises pointless error messages. Add some extra length to get past this, or leave a broader gap between your stations.
 
 				Install the street fine tuning to have useful building tools and more roads.
 
@@ -19,9 +19,9 @@ function data()
 
 				Use the categories in the street menu so you don't go bananas selecting road types.
 
-				You can use modded street types, as long as they have one of the following categories: country, highway, one-way, urban, mining. Mods can be added and removed between save games, so you have to pay a little price for flexibility: if you want to reconfigure an existing lorry station, you may have to fix its street type. Fail to do so, and you might get unwanted collision warnings.
+				You can use modded street types, as long as they have one of the following categories: country, highway, one-way, urban, mining.
 
-				If you attempt to configure your station and see collision warnings flash, make sure the right street type is selected in the configuration menu. It is a dynamic selection and it can do that, it's only cosmetic.
+				If you attempt to configure your station and see collision warnings, make sure the right street type is selected in the configuration menu. It is a dynamic selection and it can do that, it's only cosmetic.
 				
 				If you are not happy with a street mod, remove or reconfigure the modular lorry stations that use it, remove or replace all the modded streets, quit the game and reload it without the offending mod.
 
