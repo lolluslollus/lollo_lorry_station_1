5674eda397265a10c7432c594029d381c0dd8f36
lolluslollus <lolluslollus@users.noreply.github.com>
2021-09-01 15:55:17 +0200

    clean

--- res/config/game_script/lollo_lorry_station.lua
+++ res/config/game_script/lollo_lorry_station.lua
@@ -1,8 +1,4 @@
-local arrayUtils = require('lollo_lorry_station.arrayUtils')
-local moduleHelpers = require('lollo_lorry_station.moduleHelpers')
-local streetUtils = require('lollo_lorry_station.streetUtils')
-
--- LOLLO TODO try and get rid of all game. dependencies
+local _eventId = '__lolloLorryStationEvent__'
 
 local function _isBuildingConstructionWithFileName(param, fileName)
     local toAdd =
@@ -75,7 +71,6 @@ local function _replaceStationWithSnappyCopy(oldConstructionId)
     -- }
 
     -- if I bulldoze here, the station will inherit the old name
-    -- game.interface.bulldoze(oldConstructionId)
 
     local callback = function(res, success)
         -- print('LOLLO _buildStation res = ')
@@ -99,66 +94,12 @@ local function _replaceStationWithSnappyCopy(oldConstructionId)
 	api.cmd.sendCommand(cmd, callback)
 end
 
---[[ local function _replaceStationWithStreetType_(oldConstructionId)
-    -- print('oldConstructionId =', oldConstructionId)
-    if type(oldConstructionId) ~= 'number' or oldConstructionId < 0 then return end
-
-    local oldConstruction = api.engine.getComponent(oldConstructionId, api.type.ComponentType.CONSTRUCTION)
-    -- print('oldConstruction =')
-    -- debugPrint(oldConstruction)
-    if not(oldConstruction)
-    or not(oldConstruction.params)
-    or oldConstruction.params.snapNodes == 1
-    or oldConstruction.fileName ~= 'station/street/lollo_lorry_bay_with_edges.con' then return end
-
-    local newConstruction = api.type.SimpleProposal.ConstructionEntity.new()
-    newConstruction.fileName = oldConstruction.fileName
-    -- cannot clone this userdata dynamically, coz it won't take pairs and ipairs
-    local newStreetTypeIndex = moduleHelpers.getDefaultStreetTypeIndex(
-        streetUtils.getGlobalStreetData(streetUtils.getStreetDataFilters().STOCK)
-    )
-    print('newStreetTypeIndex =')
-    debugPrint(newStreetTypeIndex)
-    newConstruction.params = {
-        streetType_ = newStreetTypeIndex,
-        isStoreCargoOnPavement = oldConstruction.params.isStoreCargoOnPavement,
-        direction = oldConstruction.params.direction,
-        snapNodes = 1,
-        tramTrack = oldConstruction.params.tramTrack,
-        extraLength = oldConstruction.params.extraLength,
-        seed = oldConstruction.params.seed + 1,
-    }
-    newConstruction.transf = oldConstruction.transf
-    -- some dummy name, it will be overwritten if I bulldoze before building anew
-    newConstruction.name = 'LOLLO snapping lorry bay'
-    newConstruction.playerEntity = api.engine.util.getPlayer()
-
-    local proposal = api.type.SimpleProposal.new()
-    proposal.constructionsToAdd[1] = newConstruction
-    -- LOLLO NOTE different tables are handled differently.
-    -- This one requires this system, UG says they will document it or amend it.
-    proposal.constructionsToRemove = { oldConstructionId }
-
-    local callback = function(res, success)
-	end
-
-    local context = api.type.Context:new()
-    context.checkTerrainAlignment = false -- true gives smoother z, default is false
-    context.cleanupStreetGraph = false -- default is false
-    context.gatherBuildings = false -- default is false
-    context.gatherFields = true -- default is true
-    context.player = api.engine.util.getPlayer()
-
-	local cmd = api.cmd.make.buildProposal(proposal, context, true) -- the 3rd param is "ignore errors"
-	api.cmd.sendCommand(cmd, callback)
-end ]]
-
 function data()
     return {
-        ini = function()
-        end,
+        -- ini = function()
+        -- end,
         handleEvent = function(src, id, name, param)
-            if (id ~= '__lolloLorryStationEvent__') then return end
+            if (id ~= _eventId) then return end
             if type(param) ~= 'table' or type(param.constructionEntityId) ~= 'number' or param.constructionEntityId < 0 then return end
 
             -- print('param.constructionEntityId =', param.constructionEntityId or 'NIL')
@@ -170,82 +111,29 @@ function data()
         end,
         guiHandleEvent = function(id, name, param)
             -- LOLLO NOTE param can have different types, even boolean, depending on the event id and name
-            if id == 'constructionBuilder' and name == 'builder.apply' then
-                xpcall(
-                    function()
-                        if not param.result or not param.result[1] then
-                            return
-                        end
+            if (id == 'constructionBuilder' and name == 'builder.apply') then
+                -- xpcall(
+                --     function()
+                        if (not(param.result) or not(param.result[1])) then return end
 
                         if _isBuildingLorryBayWithEdges(param) then
-                            game.interface.sendScriptEvent(
-                                '__lolloLorryStationEvent__',
+                            api.cmd.sendCommand(api.cmd.make.sendScriptEvent(
+                                string.sub(debug.getinfo(1, 'S').source, 1),
+                                _eventId,
                                 'lorryStationBuilt',
                                 {
                                     constructionEntityId = param.result[1]
                                 }
-                            )
+                            ))
                         end
-                    end,
-                    _myErrorHandler
-                )
-            --[[ elseif name == 'select' then
-                print('guiHandleEvent firing, id =', id, 'name =', name, 'param =')
-                debugPrint(param)
-
-                if type(param) == 'number' then
-                    local entity = game.interface.getEntity(param)
-
-                    if (entity and entity.type == "STATION_GROUP") then
-                        local allLorryStationConstructions = game.interface.getEntities(
-                            {pos = entity.position, radius = 999},
-                            {type = "CONSTRUCTION", includeData = true, fileName = "station/street/lollo_lorry_bay_with_edges.con"}
-                        )
-                        print('allLorryStationConstructions =')
-                        debugPrint(allLorryStationConstructions)
-
-                        -- the game distinguishes constructions, stations and station groups.
-                        -- Constructions and stations in a station group are not selected, only the station group itself,
-                        -- which does not contain a lot of data: this is why we need this loop.
-                        -- The API here does not help, the old game.interface is better.
-
-                        -- LOLLO NOTE try this api instead:
-                        -- api.engine.system.streetConnectorSystem.getConstructionEntityForStation(stations)
-                        for _, staId in ipairs(entity.stations) do
-                            for _, con in pairs(allLorryStationConstructions) do
-                                if arrayUtils.arrayHasValue(con.stations, staId) then
-                                    print('found con =')
-                                    debugPrint(con)
-                                    if con.id and con.params and con.params.streetType_ then
-                                        local allStreetData = streetUtils.getGlobalStreetData(streetUtils.getStreetDataFilters().STOCK)
-
-                                        print('#allStreetData =', #allStreetData)
-                                        if con.params.streetType_ > #allStreetData then
-                                            game.interface.sendScriptEvent(
-                                                "__lolloLorryStationEvent__",
-                                                "lorryStationSelected",
-                                                {
-                                                    constructionEntityId = con.id,
-                                                    constructionParams = con.params
-                                                }
-                                            )
-                                        end
-                                    end
-                                end
-                            end
-                        end
-                    end
-                end ]]
+                --     end,
+                --     _myErrorHandler
+                -- )
             end
         end,
-        update = function()
-        end,
-        guiUpdate = function()
-        end,
-        -- save = function()
-        --     return allState
+        -- update = function()
         -- end,
-        -- load = function(allState)
-        -- end
+        -- guiUpdate = function()
+        -- end,
     }
 end
